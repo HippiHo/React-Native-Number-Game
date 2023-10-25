4cb8705083a303ee9b5d7f799c97a8215f7876a5
HippiHo <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>
2023-10-25 22:28:43 +0200

    adds final components and some styling

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,11 +1,12 @@
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View, Image, Text } from "react-native";
 
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 function GameOverScreen() {
   return (
-    <View>
+    <View style={styles.rootContainer}>
       <Title>Game Over!</Title>
       <View style={styles.imageContainer}>
         <Image
@@ -14,6 +15,11 @@ function GameOverScreen() {
           source={require("../assets/images/success.png")}
         />
       </View>
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>x</Text> rounds to
+        guess the number <Text style={styles.highlight}>y</Text>.
+      </Text>
+      <PrimaryButton>Start New Game</PrimaryButton>
     </View>
   );
 }
@@ -21,19 +27,35 @@ function GameOverScreen() {
 export default GameOverScreen;
 
 const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   imageContainer: {
-    width: 400,
-    height: 400,
+    width: 300,
+    height: 300,
     // natively to create a circle we set the border radius to half of our square!
-    borderRadius: 200,
+    borderRadius: 150,
     borderWidth: 3,
     borderColor: Colors.primary800,
     overflow: "hidden",
     margin: 36,
-    alignItems: 'center'
+    alignItems: "center",
   },
   image: {
-    height: '100%', 
-    width: '100%',
+    height: "100%",
+    width: "100%",
+  },
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
   },
 });
